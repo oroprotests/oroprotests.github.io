4200c4d1b87ec51481fd5ab64aa04e4807e2ac15
Oro Protests <beekaatuma@gmail.com>
2021-02-21 23:38:12 +0300

    initial build

---
+++ precache-manifest.2ef2db56e74702f6aea35abbd1453936.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a559799daa80ab97eeea",
+    "url": "/static/css/main.aa527d8f.chunk.css"
+  },
+  {
+    "revision": "a559799daa80ab97eeea",
+    "url": "/static/js/main.a559799d.chunk.js"
+  },
+  {
+    "revision": "300b1eafc0168e18e817",
+    "url": "/static/js/1.300b1eaf.chunk.js"
+  },
+  {
+    "revision": "719c9c95874294ae9606",
+    "url": "/static/js/2.719c9c95.chunk.js"
+  },
+  {
+    "revision": "46063227c0ce83095f09",
+    "url": "/static/js/runtime~main.46063227.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "109224d7ba5a0f2b5ad41a5a740234d7",
+    "url": "/index.html"
+  }
+];
